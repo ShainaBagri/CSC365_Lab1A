5b7bff1be9353e564c661ed2a0bd16cfb8a84b03
pipfeld <starstruckcountry@gmail.com>
2021-01-11 15:45:25 -0800

    busSearch

--- schoolsearch.py
+++ schoolsearch.py
@@ -9,12 +9,14 @@ def lastNameBusSearch(df, lastName):
 def teacherSearch(df, TLName, TFName):
     print(df[(df['TLastName']==TLName) & (df['TFirstName']==TFName)])
 
+def busSearch(df, busNum):
+    print(df.loc[df['Bus'] == busNum])
 
 def main():
     data_dir = "students.txt"
     df_students = pd.read_csv(data_dir, header=None, names=['StLastName', 'StFirstname', 
         'Grade', 'Classroom', 'Bus', 'GPA', 'TLastName', 'TFirstName'])
-    teacherSearch(df_students, "HAMER", "GAVIN")
+    busSearch(df_students, 52)
 
 if __name__ == "__main__":
     main()
