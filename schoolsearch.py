29de6151869ea645ef4d8676a397a7c0da30f911
pipfeld <starstruckcountry@gmail.com>
2021-01-11 15:28:33 -0800

    newest changes before liveshare fail

--- schoolsearch.py
+++ schoolsearch.py
@@ -6,14 +6,16 @@ def lastNameSearch(df, lastName):
 def lastNameBusSearch(df, lastName):
     df_new = df.loc[df['StLastName' == lastName]]
 
-def teacherSearch(df, TFName, TLName):
-    print(df.loc[df[('TLastName' == TLName) and ('TFirstName' == TFName)]])
+def teacherSearch(df, TLName, TFName):
+    #print(df.query('TLastName=="TLName" & TFirstName=="TFName" ', inplace=True))
+    print(df.loc[df['TLastName' == TLName], df['TFirstName' == TFName]])
+
 
 def main():
     data_dir = "students.txt"
     df_students = pd.read_csv(data_dir, header=None, names=['StLastName', 'StFirstname', 
         'Grade', 'Classroom', 'Bus', 'GPA', 'TLastName', 'TFirstName'])
-    teacherSearch(df_students, "Rocio", "Fafard")
+    teacherSearch(df_students, "HAMER", "GAVIN")
 
 if __name__ == "__main__":
     main()
