a270933c12797aced8eae56dcac2684444f5434a
ShainaBagri <shainabagri@gmail.com>
2021-01-11 15:43:41 -0800

    lastNameBusSearch

--- schoolsearch.py
+++ schoolsearch.py
@@ -5,6 +5,8 @@ def lastNameSearch(df, lastName):
 
 def lastNameBusSearch(df, lastName):
     df_new = df.loc[df['StLastName' == lastName]]
+    for index, row in df_new.iterrows():
+        print(index, row)
 
 def teacherSearch(df, TLName, TFName):
     #print(df.query('TLastName=="TLName" & TFirstName=="TFName" ', inplace=True))
@@ -15,7 +17,7 @@ def main():
     data_dir = "students.txt"
     df_students = pd.read_csv(data_dir, header=None, names=['StLastName', 'StFirstname', 
         'Grade', 'Classroom', 'Bus', 'GPA', 'TLastName', 'TFirstName'])
-    teacherSearch(df_students, "HAMER", "GAVIN")
+    lastNameBusSearch(df_students, "Corker")
 
 if __name__ == "__main__":
     main()
