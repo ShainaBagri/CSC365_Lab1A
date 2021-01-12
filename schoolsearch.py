43ca3b9508915fcf6c3124a01db84ffd213f92d2
pipfeld <starstruckcountry@gmail.com>
2021-01-11 18:30:04 -0800

    searches working

--- schoolsearch.py
+++ schoolsearch.py
@@ -6,8 +6,8 @@ def lastNameSearch(df, lastName):
 def lastNameBusSearch(df, lastName):
     print(df.loc[df['StLastName'] == lastName, ['StLastName', 'StFirstName', 'Bus']])
 
-def teacherSearch(df, TLName, TFName):
-    print(df[(df['TLastName']==TLName) & (df['TFirstName']==TFName)])
+def teacherSearch(df, TLName):
+    print(df[df['TLastName']==TLName])
 
 def busSearch(df, busNum):
     print(df.loc[df['Bus'] == busNum])
@@ -25,21 +25,48 @@ def lowestGPA(df, grade):
 
 def highestGPA(df, grade):
     new_df = df.loc[df['Grade'] == grade]
-    print(new_df["GPA"].max())
+    print("Min GPA: " + new_df["GPA"].max())
 
 def main():
     data_dir = "students.txt"
     df_students = pd.read_csv(data_dir, header=None, names=['StLastName', 'StFirstName', 
         'Grade', 'Classroom', 'Bus', 'GPA', 'TLastName', 'TFirstName'])
-<<<<<<< HEAD
-    highest(df_students, 2)
-=======
-<<<<<<< HEAD
-    lastNameBusSearch(df_students, "CORKER")
-=======
-    gradeSearch(df_students, 2)
->>>>>>> 579c90dd7692dadbdbd6e42098900ccc3d5d7134
->>>>>>> c19db5dcb9b461eed6c650643912a1597690b978
+    
+    quit = False
+    while not quit:
+        command = str(input("Type your command: "))
+        split = command.split()
+        if split[0]=="S:" or split[0]=="Student:":
+            lastName = split[1]
+            if len(split) != 3:
+                lastNameSearch(df_students, lastName)
+            elif len(split) == 3 and (split[2] == "B" or split[2] == "Bus"):
+                lastNameBusSearch(df_students, lastName)
+        elif split[0]=="Teacher:" or split[0]=="T:":
+            lastName = split[1]
+            teacherSearch(df_students, lastName)
+        elif split[0]=="Grade:" or split[0]=="G:":
+            number = split[1]
+            gradeSearch(df_students, number)
+        elif split[0]=="B:" or split[0]=="Bus:":
+            number = split[1]
+            busSearch(df_students, number)
+        elif split[0]=="Grade:" or split[0]=="G:":
+            number = split[1]
+            if split[2]=="High" or split[2]=="H":
+                highestGPA(df_students, number)
+            elif split[2]=="Low" or split[2]=="L":
+                lowestGPA(df_students, number)
+        elif split[0]=="Q" or split[0]=="Quit":
+            quit = True
+
+
+
+                
+                
+
+
+
 
 if __name__ == "__main__":
     main()
