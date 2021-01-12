9f6fa267adc883ae100af226aa8a58bd36e10854
pipfeld <starstruckcountry@gmail.com>
2021-01-11 23:16:44 -0800

    writeup

--- schoolsearch.py
+++ schoolsearch.py
@@ -1,41 +1,70 @@
 import pandas as pd
 
 def lastNameSearch(df, lastName):
-    print(df.loc[df['StLastName'] == lastName,
-        ['StLastName', 'StFirstName', 'Grade', 'Classroom', 'TLastName', 'TFirstName']])
+    new_df = df.loc[df['StLastName'] == lastName,
+        ['StLastName', 'StFirstName', 'Grade', 'Classroom', 'TLastName', 'TFirstName']]
+    if new_df.empty:
+        print("No student with this last name exists")
+    else:
+        print(new_df)
 
 def lastNameBusSearch(df, lastName):
-    print(df.loc[df['StLastName'] == lastName, 
-        ['StLastName', 'StFirstName', 'Bus']])
+    new_df = df.loc[df['StLastName'] == lastName, 
+        ['StLastName', 'StFirstName', 'Bus']]
+    if new_df.empty:
+        print("No student with this last name exists")
+    else:
+        print(new_df)
 
-def teacherSearch(df, TLName, TFName):
-    print(df.loc[(df['TLastName']==TLName) & (df['TFirstName']==TFName), 
-        ['StLastName', 'StFirstName']])
+def teacherSearch(df, TLName):
+    new_df = df.loc[df['TLastName']==TLName, 
+        ['StLastName', 'StFirstName']]
+    if new_df.empty:
+        print("No teacher with this last name exists")
+    else:
+        print(new_df)
 
 def busSearch(df, busNum):
-    print(df.loc[df['Bus'] == busNum,
-        ['StLastName', 'StFirstName', 'Grade', 'Classroom']])
+    new_df = df.loc[df['Bus'] == busNum,
+        ['StLastName', 'StFirstName', 'Grade', 'Classroom']]
+    if new_df.empty:
+        print("Bus route not found")
+    else:
+        print(new_df)
 
 def gradeSearch(df, grade):
-    print(df.loc[df['Grade'] == grade,
-        ['StLastName', 'StFirstName']])
+    new_df = df.loc[df['Grade'] == grade,
+        ['StLastName', 'StFirstName']]
+    if new_df.empty:
+        print("No students in this grade")
+    else:
+        print(new_df)
 
 def avgGPA(df, grade):
     new_df = df.loc[df['Grade'] == grade]
-    print("Grade \tAverage GPA")
-    print(grade, " \t", new_df["GPA"].mean())
+    if(new_df.empty):
+        print("No students in this grade")
+    else:
+        print("Grade \tAverage GPA")
+        print(grade, " \t", new_df["GPA"].mean())
 
 def lowestGPA(df, grade):
     new_df = df.loc[df['Grade'] == grade,
         ['StLastName', 'StFirstName', 'GPA', 'TLastName', 'TFirstName', 'Bus']]
-    minGPA = new_df["GPA"].min()
-    print(new_df.loc[new_df['GPA'] == minGPA])
+    if(new_df.empty):
+        print("No students in this grade")
+    else:
+        minGPA = new_df["GPA"].min()
+        print(new_df.loc[new_df['GPA'] == minGPA])
 
 def highestGPA(df, grade):
     new_df = df.loc[df['Grade'] == grade,
         ['StLastName', 'StFirstName', 'GPA', 'TLastName', 'TFirstName', 'Bus']]
-    maxGPA = new_df["GPA"].max()
-    print(new_df.loc[new_df['GPA'] == maxGPA])
+    if(new_df.empty):
+        print("No students in this grade")
+    else:
+        maxGPA = new_df["GPA"].max()
+        print(new_df.loc[new_df['GPA'] == maxGPA])
 
 def numStudents(df):
     print("0: ", len(df[df['Grade'] == 0]))
@@ -118,6 +147,7 @@ def main():
     #highestGPA(df_students, 3)
     #highestGPA(df_students, 1)
     #numStudents(df_students)
+    
 
 if __name__ == "__main__":
     main()
